56c1167de742bf3f6276ae0a80d903f9b6c7200f
King Amato <84693218+chaexd14@users.noreply.github.com>
2025-10-11 00:59:24 +0800

    Update layout.jsx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
         />
       </head>
 
-      <body>
+      <body classname="bg-[#0E0E0E]">
         {children}
         <footer className="py-4 text-center text-[#D9D9D9] bg-[#0E0E0E] border-t border-b border-[#B7B5C2] desktop:text-xl">
           © 2025 King Amato. All rights reserved.
